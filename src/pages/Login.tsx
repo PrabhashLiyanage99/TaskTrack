1155414ade6acbb6182a9459da63ed74f385d4c8
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2025-02-28 00:00:51 +0530

    chore: Initial project setup

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500">
+      <h1 className="text-3xl font-bold text-blue-600">Login! 🚀</h1>
+    </div>
+  )
+}
+
+export default Login
