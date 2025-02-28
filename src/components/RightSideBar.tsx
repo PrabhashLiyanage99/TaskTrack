44debfddb997e0dc4ce64ce08471f31acfce54b4
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2025-02-28 17:40:15 +0530

    feat: Add task charts

---
+++ src/components/RightSideBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RightSidebar: React.FC = () => {
+    return (
+        <div className="h-screen w-60 bg-gray-800 text-white flex flex-col p-4 shadow-lg">
+
+        </div>
+    );
+};
+
+export default RightSidebar;
