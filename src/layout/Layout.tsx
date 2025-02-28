ecff7f8fb661f6b7574e71cf4c7fe1901a66c612
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2025-02-28 18:56:00 +0530

    fix: Resolve type error and fix the layout issue

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from "react";
+import RightSidebar from "../components/RightSideBar";
+import Header from "../components/Header";
+
+interface LayoutProps {
+    children: ReactNode;
+  }
+  
+  const Layout: React.FC<LayoutProps> = ({ children }) => {
+    return (
+      <div className="flex h-screen bg-gray-100">
+
+        <RightSidebar />
+        <div className="flex-1 flex flex-col">
+          <Header />
+          
+          <main className="flex-1 p-6">{children}</main>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Layout;
