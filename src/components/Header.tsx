1f0ea8d1897c3178abfc0cc48c12c4d840fe65d6
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2025-03-04 21:44:38 +0530

    style: enhance UI for all displays

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,29 @@
 import { useAuth } from "../context/AuthContext";
 import { Link } from 'react-router-dom'
+import logo from '../assets/Tasktrack_logo.png';
+import off from '../assets/off.svg';
 
 const Header = () => {
     const { logout } = useAuth();
 
     return (
         <header className="fixed top-0 left-0 w-full bg-gray-900 text-white p-4 shadow-md flex justify-between items-center z-50">
-            <Link
-                to="/todo"
-                className="px-4 py-2 rounded hover:bg-gray-700 transition">
-                ToDo
-            </Link>
+            <div className="flex lg:gap-12 sm:gap-2">
+                <Link
+                    to="/todo"
+                    className=" rounded hover:bg-gray-700 transition">
+                    <img src={logo} alt="logo" className="w-44 h-10"/>
+                </Link>
+                <Link
+                    to="/todo"
+                    className="px-4 py-2 rounded hover:bg-gray-700 transition">
+                    ToDo
+                </Link>
+            </div>
             <button 
                 onClick={logout}
-                className="bg-read-500 px-4 py-2 rounded hover:bg-red-700 transition">
+                className="bg-read-500 px-4 py-2 rounded hover:bg-red-700 transition flex gap-2">
+                <img src={off} alt="" className="w-6 h-6 "/>
                 Logout
             </button>
         </header>
--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -9,14 +9,19 @@ interface PopupProps {
 const Popup: React.FC<PopupProps> = ({ message, onConfirm, onCancel }) => {
     return (
         <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
-      <div className="bg-white p-6 rounded shadow-lg text-center">
+      <div className="bg-gray-500 p-6 pt-2 rounded shadow-lg text-end">
+      <button type="button" onClick={onCancel} className="text-white hover:text-red-500 text-xl text-end mb-2 ">
+            ✖
+          </button>
         <p className="mb-4">{message}</p>
+        <div className="text-center">
         <button className="bg-orange-500 text-white px-4 py-2 rounded mr-2" onClick={onConfirm}>
           Yes
         </button>
-        <button className="bg-gray-500 text-white px-4 py-2 rounded" onClick={onCancel}>
+        <button className="hover:bg-red-500 text-white px-4 py-2 rounded" onClick={onCancel}>
           No
         </button>
+        </div>
       </div>
     </div>
     );
